8fdc9840e9a74f3fdc1972a1c337cc8f3e6d5539
David Edler <davidedler@Davids-MacBook-Air.local>
2025-01-15 22:23:08 -0700

    Serve index.html from templates folder

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from your Docker-based Flask app!"
+    # This calls 'index.html' from the 'templates/' folder
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Listen on port 8080
+    # Make sure we match the port your Dockerfile expects (8080)
     app.run(host='0.0.0.0', port=8080, debug=True)
+
 
 
